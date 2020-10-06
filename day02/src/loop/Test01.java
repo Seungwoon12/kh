6020a4ae5ee3802a6ce1e413ef4148c761a3c3f8
Seungwoon <Seungwoon@DESKTOP-M3QL7KG>
2020-10-06 19:28:47 +0900

    hello 200번

--- day02/src/loop/Test01.java
+++ day02/src/loop/Test01.java
@@ -2,7 +2,8 @@ package loop;
 
 public class Test01 {
 	public static void main(String[] args) {
-		System.out.println("테스트출력");
-		System.out.println('1');
-	}
+		for(int i=0 ; i < 200 ; i ++) {
+			System.out.println("hello");
+		}
+}
 }
