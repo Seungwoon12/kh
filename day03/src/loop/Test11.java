93284006b2fb9d2dd9c1385663e06c6ea9bbf1ff
Seungwoon <Seungwoon@DESKTOP-M3QL7KG>
2020-10-08 16:01:28 +0900

    day03 업뎃

--- day03/src/loop/Test11.java
+++ day03/src/loop/Test11.java
@@ -2,11 +2,14 @@ package loop;
 
 public class Test11 {
 	public static void main(String[] args) {
-		int sum = 0;
-		int day1 = 10;
-		int days;
-		for(int i=1 ; i<=30 ; i++) {
-			sum += 
+		int total = 0;
+		int pushup = 10;
+		
+		for(int i=1; i<=30; i++) {
+			total += pushup;
+			pushup += 3;
 		}
+		System.out.println(total
+				);
 	}
 }
