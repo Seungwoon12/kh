647ce7ea0810dbc4fc730c2202dd19beb7b4a850
Seungwoon <Seungwoon@DESKTOP-M3QL7KG>
2020-10-12 16:59:38 +0900

    day10 생성

--- day10/src/abc/Test01.java
+++
@@ -1,8 +0,0 @@
-package abc;
-
-public class Test01 {
-	public static void main(String[] args) {
-		System.out.println("ㅠㅠ");
-		System.out.println(1234);
-	}
-}
---
+++ day10/src/sort/Test01.java
@@ -0,0 +1,8 @@
+package sort;
+
+public class Test01 {
+	public static void main(String[] args) {
+		int[] data = new int[] {3, 5, 2, 1, 4};
+	}
+
+}
