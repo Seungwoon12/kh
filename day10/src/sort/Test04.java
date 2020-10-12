564713cc8285a9d4ae8144b13540abe99449f911
Seungwoon <Seungwoon@DESKTOP-M3QL7KG>
2020-10-13 00:09:42 +0900

    test04 버블정렬

--- day10/src/sort/Test04.java
+++ day10/src/sort/Test04.java
@@ -3,10 +3,30 @@ package sort;
 public class Test04 {
 	public static void main(String[] args) {
 		
-		int[] data = new int[] {30, 50, 20, 10, 40};
+		int[] data = new int[] {30, 50, 20, 10, 40,};
 		
+		for(int k=data.length-1 ; k>0 ; k--) {
+			
+			for(int i=0 ; i<k ; i++ ) {
+				if(data[i] > data[i+1]) {
+					int temp = data[i+1];
+					data[i+1] = data[i];
+					data[i] = temp;
+				}
+			}
+		}
+		
+		for(int i=0 ; i<data.length ; i++) {
+			System.out.println(data[i]);
+		}
+	
+			
+		
+		
+			
+		}
 		
 	}
-}
+
 			
 
