7238e11f9bcf8fb7acb8ff752c269aa7d15d0bfb
Seungwoon <Seungwoon@DESKTOP-M3QL7KG>
2020-10-14 17:49:47 +0900

    day11

--- day11/src/oop/basic4/Menu.java
+++ day11/src/oop/basic4/Menu.java
@@ -2,27 +2,30 @@ package oop.basic4;
 
 public class Menu {
 	
-	String name;
-	String category;
-	int price;
-	char sale;
-	
-	
-	void info(String a, String b, int c, char d) {
-		this.name = a;
-		this.category = b;
-		this.price = c;
-		this.sale = d;
-	}
-	
-				
-	void print() {
-		if(this.sale == 'O') {
-			this.price -= 500;
+	//인스턴스변수, 멤버변수 : 객체가 사라지기 전까지 존재
+		String name;
+		String category;
+		int price;
+		boolean event;
+		
+		void info(String name, String category, int price, boolean event) {
+			this.name = name;
+			this.category = category;
+			this.price = price;
+			this.event = event;
 		}
-		System.out.println("이름 : "+this.name);
-		System.out.println("분류 : "+this.category);
-		System.out.println("판매가격 : "+this.price);
-		System.out.println("행사여부 : "+this.sale);
+		
+		void print() {
+			System.out.println("## 메뉴 정보 ##");
+			System.out.println("메뉴 이름 : "+this.name);
+			System.out.println("분류 : "+this.category);
+			System.out.println("가격 : "+this.price+"원");
+			if(this.event) {//this.event가 true라면
+//			if(!this.event) {//this.event가 false라면
+				int discount = this.price - 500;//임시변수(메소드 종료시 사라짐 : 로컬,지역변수)
+				System.out.println("→ 할인가격 : "+discount+"원");
+			}
+			//System.out.println("행사여부 : "+this.event);
+		}
 	}
-}
+
--- day11/src/oop/basic4/Test01.java
+++ day11/src/oop/basic4/Test01.java
@@ -3,17 +3,19 @@ package oop.basic4;
 public class Test01 {
 	public static void main(String[] args) {
 		
-		Menu p1 = new Menu();
-		Menu p2 = new Menu();
-		Menu p3 = new Menu();
+		//객체 생성(3개)
+		Menu m1 = new Menu();
+		Menu m2 = new Menu();
+		Menu m3 = new Menu();
 		
-		p1.info("아메리카노", "음료", 1500, 'X');
-		p2.info("카페라떼", "음료", 2000, 'O');
-		p3.info("샌드위치", "식사", 3000, 'O');
+		//정보 설정(3번)
+		m1.info("아메리카노", "음료", 1500, false);
+		m2.info("카페라떼", "음료", 2000, true);
+		m3.info("샌드위치", "식사", 3000, true);
 		
-		p1.print();
-		p2.print();
-		p3.print();
-				
+		//출력(3번)
+		m1.print();
+		m2.print();
+		m3.print();
 	}
 }
