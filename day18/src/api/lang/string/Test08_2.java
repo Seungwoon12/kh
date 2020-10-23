b547c0d012298b7d72830e64f935a2579e363b41
Seungwoon <Seungwoon@DESKTOP-M3QL7KG>
2020-10-23 15:12:36 +0900

    day18 욕설 필터링

--- day18/src/api/lang/string/Test08_2.java
+++ day18/src/api/lang/string/Test08_2.java
@@ -1,26 +1,28 @@
 package api.lang.string;
 
 import java.util.Scanner;
+
 public class Test08_2 {
 //	객체없이 별을 만드는 메소드 구현
 	public static String generate(int size) {
-		String str = "";
-		for(int i=0; i<size; i++) {
-			str += "*";
-		}
-		return str;
+	String str = "";
+	for(int i=0; i<size; i++) {
+		str += "*";
 	}
+	return str;
+	
+}
 	
 	public static void main(String[] args) {
-//		필터링 프로그램
+		//필터링 프로그램
 		Scanner sc = new Scanner(System.in);
-		System.out.println("채팅 입력");
+		System.out.println("내용을 입력하세요!");
 		String input = sc.nextLine();
 		
-		String[] filter = new String[] {"시베리아","조카","신발","십장생","주옥"};
+		String[] curse = new String[] {"시베리아", "조카", "신발", "십장생", "주옥"};
 		
-		for(int i=0; i < filter.length; i++) {
-			input = input.replace(filter[i], generate(filter[i].length()));
+		for(int i=0; i<curse.length; i++) {
+			input = input.replace(curse[i], generate(curse[i].length()));
 		}
 		
 		System.out.println(input);
