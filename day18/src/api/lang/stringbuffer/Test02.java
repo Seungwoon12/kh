603c9b1c85907fa7f6439b23d7772854832380b5
Seungwoon <Seungwoon@DESKTOP-M3QL7KG>
2020-11-09 17:09:59 +0900

    day18

--- day18/src/api/lang/stringbuffer/Test02.java
+++ day18/src/api/lang/stringbuffer/Test02.java
@@ -11,11 +11,11 @@ public class Test02 {
 		long a = System.currentTimeMillis();
 		for(int i=0; i<10000000; i++) {
 			buffer.append("hello");//hello를 덧붙여라
-			buffer.append("\n");//엔터를 덧붙여라
+//			buffer.append("\n");//엔터를 덧붙여라
 		}
 		long b = System.currentTimeMillis();
 		System.out.println(b-a+"ms");//소요시간
 		
-//		System.out.println(buffer.toString());//문자열로 바꿔서 출력해라!
+		System.out.println(buffer.toString());//문자열로 바꿔서 출력해라!
 	}
 }
